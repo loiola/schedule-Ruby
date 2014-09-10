ddab215382abaf6efc4a20ba16f611deed4e23dd
Leonn <leonn.paiva@gmail.com>
2014-09-10 15:21:25 -0300

    add validates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ActiveRecord::Base
+    validates :name, presence:true 
+    validates :phone, presence:true, numericality:true}
+    validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
